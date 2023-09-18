aa2a64152b87885131b8447733f07bb4548340e2
dianakagramanyan <diana.kagramanyan1@gmail.com>
2023-09-18 12:46:48 -0400

    setting up session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe} from "@nestjs/common";
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { ValidationPipe } from "@nestjs/common";
+import { AppModule } from "./app.module";
+
+
+const cookieSession = require("cookie-session");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ["nvnbvnbv"]
+  }));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true
@@ -11,4 +17,5 @@ async function bootstrap() {
   );
   await app.listen(3000);
 }
+
 bootstrap();
